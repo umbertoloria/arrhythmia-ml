b1a664dcdc0ceca83010873f4b4274ae2f4cc694
Umberto Loria SAM <umberto.loria@gmail.com>
2021-06-26 19:59:50 +0200

    Improved dataset load, calculated kmeans with Scikit-learn

--- main.py
+++ main.py
@@ -1,108 +1,51 @@
-def get_lines(filepath):
-	file = open(filepath, 'r')
-	lines = list()
-	for line in file.readlines():
-		if line.endswith('\r\n'):
-			line = line[:-2]
-		if line.endswith('\r'):
-			line = line[:-1]
-		if line.endswith('\n'):
-			line = line[:-1]
-		lines.append(line)
-	return lines
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+from dataset_load import load_dataset
 
 
-def load_dataset(data_filepath):
-	import sys
-	import pandas as pd
-	data_lines = get_lines(data_filepath)
-	records = list()
-	for line in data_lines:
-		# parts = line.split(',')
-		# record = list()
-		record = line.split(',')
-		records.append(record)
-	# width = len(records[0])
-	# height = len(records)
-	# print(width, height)
-	columns = {
-		'age': [],
-		'sex': [],  # 0 <==> male, 1 <==> female
-		'height': [],
-		'weight': [],
-		'qrs-duration': [],  # Average of QRS duration in milliseconds
-		'pq-interval': [],  # Average duration between onset of P and Q waves in milliseconds
-		'qt-interval': [],  # Average duration between onset of Q and offset of T waves in milliseconds
-		't-interval': [],  # Average duration of T wave in milliseconds
-		'p-interval': [],  # Average duration of P wave in milliseconds
-		# Vector angles in degrees of front plane of:
-		'qrs': [],
-		't': [],
-		'p': [],
-		'qrst': [],
-		'j': [],
-	}
-	mapping_cols_configs = {
-		0: {'column': 'age', 'type': 'int'},
-		1: {'column': 'sex', 'type': 'int'},
-		2: {'column': 'height', 'type': 'int'},
-		3: {'column': 'weight', 'type': 'int'},
-		4: {'column': 'qrs-duration', 'type': 'int'},
-		5: {'column': 'pq-interval', 'type': 'int'},
-		6: {'column': 'qt-interval', 'type': 'int'},
-		7: {'column': 't-interval', 'type': 'int'},
-		8: {'column': 'p-interval', 'type': 'int'},
-		9: {'column': 'qrs', 'type': 'int'},
-		10: {'column': 't', 'type': 'int', 'optional': True},
-		11: {'column': 'p', 'type': 'int', 'optional': True},
-		12: {'column': 'qrst', 'type': 'int', 'optional': True},
-		13: {'column': 'j', 'type': 'int', 'optional': True},
-	}
-	for record in records:
-		for index, value in enumerate(record):
-			if index in mapping_cols_configs:
-				config = mapping_cols_configs[index]
-				config_column = config['column']
-				config_type = config['type']
-				if config_type == 'int':
-					try:
-						value = int(value)
-					except:
-						if 'optional' in config.keys() and config['optional']:
-							value = None
-						else:
-							print(f'ERROR: Int convertion of columns {config_column}', file=sys.stderr)
-				elif config_type == 'float':
-					try:
-						value = float(value)
-					except:
-						if 'optional' in config.keys() and config['optional']:
-							value = None
-						else:
-							print(f'ERROR: Float convertion of columns {config_column}', file=sys.stderr)
-				columns[config_column].append(value)
-	df = pd.DataFrame(data=columns)
-	return df
+def compose_points(col1, col2):
+	if len(col1) != len(col2):
+		raise Exception("Unable to build points: column sizes must be equal")
+	it1 = iter(col1)
+	it2 = iter(col2)
+	points = list()
+	for i in range(len(col1)):
+		points.append([next(it1), next(it2)])
+	return points
 
 
-def visualize_full(df):
-	print(df.head())
-	print(df.describe())
-	import seaborn as sns
-	plt = sns.pairplot(df[['age', 'height', 'weight']])
-	plt.savefig('pairplot.png')
+# K-means clustering
+def compute_kmeans(df, n_clusters, xcol, ycol, filepath):
+	kmeans_model = KMeans(n_clusters=n_clusters)
+	points = compose_points(df[xcol].values, df[ycol].values)
+	x = np.array(points)
+	kmeans_result = kmeans_model.fit(x)
+	# print(kmeans_result.cluster_centers_)
+	cluster_column = f'cluster-{xcol}-{ycol}'
+	df[cluster_column] = kmeans_result.labels_
+	plt.figure()
+	sns.scatterplot(x=xcol, y=ycol, data=df, hue=cluster_column, palette="Accent", legend=False)
+	plt.savefig(filepath)
 
 
 def main():
 	data_filepath = 'dataset/arrhythmia.data'
-	df = load_dataset(data_filepath)
-	visualize_full(df)
+	skip_records_with_empty_values = False
+	dataset = load_dataset(data_filepath, skip_records_with_empty_values)
 
+	df = pd.DataFrame(data=dataset)
+	print(df.head())
+	print(df.describe())
 
-# print(ds)
-
-
-# print(f'Hi')
+	# compute_kmeans(df, 2, 't', 'p', 'plots/scatterplot-t-p.png')
+	compute_kmeans(df, 2, 'pq-interval', 'qt-interval', 'plots/scatterplot-pqinterval-qtinterval.png')
+	compute_kmeans(df, 3, 'weight', 'qrs-duration', 'plots/scatterplot-weight-qrsduration.png')
+	plot = sns.pairplot(df[['age', 'height', 'weight']])
+	plot.savefig('plots/pairplot.png')
 
 
 if __name__ == '__main__':
